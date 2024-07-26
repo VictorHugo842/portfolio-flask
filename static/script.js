953c7896d798dd859dc2622be1b042c643125003
VictorHugo842 <victor99.santos@gmail.com.br>
2024-07-26 14:55:05 -0300

    sobre e currículo

--- static/script.js
+++ static/script.js
@@ -1,13 +1,23 @@
-/* menu lateral responsivo */
-
-const menuMobile = document.querySelector(".menu-mobile")
-const body = document.querySelector("body")
-
+/* abre e fecha o menu lateral */
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
 
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
-    ? menuMobile.classList.replace("bi-list","bi-x") /* if */
-    : menuMobile.classList.replace("bi-x","bi-list"); /* else */
+        ? menuMobile.classList.replace("bi-list", "bi-x") /* if */
+        : menuMobile.classList.replace("bi-x", "bi-list"); /* else */
+    body.classList.toggle("menu-nav-active"); /* volta o menu */
+});
 
-    body.classList.toggle("menu-nav-active") /* volta o header */
+
+/* desabilita menu ao clicar em item e muda icone para list */ 
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
 })

d6ecaad1e8a9ef5148714264b88df1e65b0c0b9e
VictorHugo842 <victor99.santos@gmail.com.br>
2024-09-21 23:35:46 -0300

    index.

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from contato import Contato
 from dotenv import load_dotenv
 import os
 import requests
-load_dotenv()
 import logging
+
+load_dotenv()
 
 # cria a instância do aplicativo Flask
 app = Flask(__name__)
@@ -60,56 +61,6 @@ def send():
 		mail.send(msg)
 		flash("Mensagem enviada com sucesso!")
 	return redirect("/")
-
-# @app.route("/geolocalizacao", methods=["GET"])
-# def geolocalizacao():
-
-# 	if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-# 		user_ip = request.environ['REMOTE_ADDR']
-# 	else:
-# 		user_ip = request.environ['HTTP_X_FORWARDED_FOR'].split(',')[0].strip()
-
-# 	token = os.getenv("USERLOCATION")
-# 	ip_address = request.args.get('ip', user_ip)
-	
-# 	headers = {
-# 		'Authorization': f'Bearer {token}',
-# 		'Content-Type': 'application/json'
-# 	}
-
-# 	# faz a requisição à API de geolocalização
-# 	url = f"https://api.invertexto.com/v1/geoip/{ip_address}"
-# 	response = requests.get(url, headers=headers)
-
-# 	#app.logger.debug(f"Status Code: {response.status_code}")
-# 	#app.logger.debug(f"Response Text: {response.text}")
-
-# 	if response.status_code == 200:
-# 		data = response.json()  # converte a resposta em JSON
-# 		return jsonify(data)  # retorna os dados em formato JSON
-# 	else:
-# 		return response.text
-
-@app.route('/geolocalizacao', methods=['POST','GET'])
-def receive_location():
-	return render_template("teste.html")
-
-	# print(request)
-
-	# data = request.get_json()
-	# latitude = data.get('latitude')
-	# longitude = data.get('longitude')
-
-	# if latitude is None or longitude is None:
-	# 	return jsonify({'error': 'Latitude and Longitude are required'}), 400
-
-	# # Processar a localização conforme necessário
-	# response = {
-	# 	'message': 'Location received',
-	# 	'latitude': latitude,
-	# 	'longitude': longitude
-	# }
-	# return jsonify(response), 200
 
 
 # verifica se o script está sendo executado diretamente
